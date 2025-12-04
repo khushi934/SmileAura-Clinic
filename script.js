b38fbdf2baf6e8a877deabca3e9d2ef3f632666d
khushi934 <khushichaudharybst0@gmail.com>
2025-12-04 23:25:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("appointmentForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // redirect to thank-you page
+    window.location.href = "https://example.com/thank-you";
+});
